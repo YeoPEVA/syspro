97073f56537563cd5904e9cf916eff569a6ea9c5
YeoPEVA <lcy606@naver.com>
2024-10-07 12:04:32 +0900

    chap5 - 불필요한 파일 제거 / studnet.txt 파일 -> 용량 문제로 제거 chap5 - hw1, hw2 작업 진행 중...

---
+++ chap5/hw2/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#define BUFSIZE 1024
+
+char savedText[10][100];
+
+int main(int argc, char* argv[]){
+
+    printf("Check. \n");
+
+    return 0; 
+}
