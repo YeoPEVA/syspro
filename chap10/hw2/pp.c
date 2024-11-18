1eeb10b534d2841c576ed665ea95a1325b4b4f69
YeoPEVA <lcy606@naver.com>
2024-11-18 10:35:54 +0900

    chap10 hw2 end

--- chap10/hw2/pp.c
+++ chap10/hw2/pp.c
@@ -7,21 +7,38 @@ struct node {
 	struct node *next;
 };
 
-void addq(struct node *head, struct node *tail, int data){
+void addq(struct node **head, struct node **tail, int data) {
     struct node *new_node = (struct node *)malloc(sizeof(struct node));
     if (new_node == NULL) {
         perror("malloc");
         exit(1);
     }
     new_node->data = data;
+    new_node->next = NULL;
 
+    if (*tail == NULL) { 
+        *head = *tail = new_node;
+    } else {
+        (*tail)->next = new_node;
+        *tail = new_node;
+    }
 }
 
+int delete(struct node **head, struct node **tail) {
+    if (*head == NULL) { 
+        perror("Queue is empty\n");
+        exit(1);
+    }
+    struct node *temp = *head;
+    int data = temp->data;
+    *head = (*head)->next;
 
-int delete(struct node *head, struct node *tail){
-
+    if (*head == NULL) { 
+        *tail = NULL;
+    }
+    free(temp);
+    return data;
 }
-
 
 int main() 
 { 
