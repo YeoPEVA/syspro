01b26759b49578f42f56eccb95d02017ca0f8b31
YeoPEVA <lcy606@naver.com>
2024-11-25 10:42:26 +0900

    chap11 hw1 end

--- chap11/hw1/almhandler.c
+++ chap11/hw1/almhandler.c
@@ -3,25 +3,48 @@
 #include <signal.h>
 #include <unistd.h>
 
-void alarmHandler();
-/* 알람 시그널을 처리한다. */
+/// testSignal 
+// signo - 설정할 시그널 번호
+// handler - 새로 설정할 시그널 핸들러 함수
+// 반환값 - 이전에 설정된 시그널 핸들러 함수 포인터
 
+void (*testSignal(int signo, void (*handler)(int)))(int) {
+    // sigaction 구조체 
+    struct sigaction act;
+    struct sigaction old_act;
 
-int main( )
-{
-  signal(SIGALRM, alarmHandler);
-  alarm(5);    /* 알람 시간 설정 */
-  short i = 0;
-  while (1) {
-    sleep(1);
-    i++;
-    printf("%d second\n", i);
-  }
-  printf("end\n");
+    // 새로 설정할 시그널 핸들러 지정 
+    act.sa_handler = handler;
+    // 시그널 마스크 초기화 
+    sigemptyset(&act.sa_mask);
+    // 추가 플래그 -> 사용 X 
+    act.sa_flags = 0;
+
+    // sigaction - 시그널 핸들러 설정 
+    if (sigaction(signo, &act, &old_act) < 0) {
+        perror("sigaction error");
+        exit(1);
+    }
+    // 이전 핸들러 반환 
+    return old_act.sa_handler;
 }
 
-void alarmHandler(int signo)
-{
-   printf("Wake up\n");
-   exit(0);
+// 알람 시그널을 처리하는 핸들러 
+void alarmHandler(int signo) {
+    printf("Wake up\n");
+    exit(0);
+}
+
+int main() {
+    testSignal(SIGALRM, alarmHandler);
+    alarm(5);
+    
+    short i = 0;
+    while (1) {
+        sleep(1);
+        i++;
+        printf("%d second\n", i);
+    }
+    
+    printf("end\n");
 }
